60ba9568e0dbd4c133a7c4e3cc1ae63a3fc950ff
arunraj-ar <mail.arunrajar@gmail.com>
2024-09-25 23:53:59 +0530

    share page starting

--- food-order-app/app/meals/page.js
+++ food-order-app/app/meals/page.js
@@ -1,10 +1,26 @@
 import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
 
 const MealsPage = () => {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}> Meals Page </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favourite recepie and cook it yourself.</p>
+        <p className={classes.cta}>
+          {" "}
+          <Link href="/meals/share">Share your favourite recepie.</Link>
+        </p>
+      </header>
+
+      <main>
+        <h1 style={{ color: "white", textAlign: "center" }}> Meals Page </h1>
+      </main>
+    </>
   );
 };
 
