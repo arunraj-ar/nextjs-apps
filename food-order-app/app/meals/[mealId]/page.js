fd466c2e2e045f9ee9805a90c8b04d84cc2449ae
arunraj-ar <mail.arunrajar@gmail.com>
2024-09-30 20:20:08 +0530

    error and not found pages added

--- food-order-app/app/meals/[mealId]/page.js
+++ food-order-app/app/meals/[mealId]/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const MealDetails = ({ params }) => {
   return (
     <main>
-      <h1 style={{ color: "white", textAlign: "center" }}> Meals Details </h1>
+      <h1 style={{ color: "white", textAlign: "center" }}> Meal Details </h1>
       <p style={{ color: "white", textAlign: "center" }}>{params.mealId}</p>
     </main>
   );
